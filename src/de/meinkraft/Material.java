dd5e991ab32d4323c81ab638608cda8e7162fa18
Schulzi@Tutorials <->
2015-08-17 15:20:13 +0200

    Added temp variables and temp material

--- src/de/meinkraft/Material.java
+++ src/de/meinkraft/Material.java
@@ -2,8 +2,21 @@ package de.meinkraft;
 
 public class Material {
 	
+	public static final Material AIR = new Material().setOpaque(false);
+	
+	private boolean opaque;
+	
 	public Material() {
 		
+	}
+	
+	public boolean isOpaque() {
+		return opaque;
+	}
+	
+	public Material setOpaque(boolean opaque) {
+		this.opaque = opaque;
+		return this;
 	}
 	
 }
